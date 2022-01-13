7008f3bb969d82aac92fdf00849be394a707341d
Jonas Fredriksson <jonas.fredriksson94@gmail.com>
2022-01-13 21:41:25 +0100

    newDay script got some echo

--- newDay.sh
+++ newDay.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cp src/test/java/adventofcode/TestDayTemplate.java src/test/java/adventofcode/TestDay$1.java
+echo "Created file: src/test/java/adventofcode/TestDay$1.java"
 sed -i "s/Template/$1/g" src/test/java/adventofcode/TestDay$1.java
 mkdir src/main/java/adventofcode/day$1
+echo "Created folder: src/main/java/adventofcode/day$1"
 cp src/main/java/adventofcode/dayTemplate/DayTemplate.java src/main/java/adventofcode/day$1/Day$1.java
+echo "Created file: src/main/java/adventofcode/day$1/Day$1.java"
 sed -i "s/Template/$1/g" src/main/java/adventofcode/day$1/Day$1.java
 touch src/main/resources/testinput$1.txt
+echo "Created file: src/main/resources/testinput$1.txt"
 touch src/main/resources/input$1.txt
+echo "Created file: src/main/resources/input$1.txt"
